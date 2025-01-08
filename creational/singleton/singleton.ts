5c2fc61a86709bd9f376309722ae3d95a979c231
Pedro Lugon <pedromotta20@gmail.com>
2025-01-08 18:00:50 -0300

    refac: singleton

--- creational/singleton/singleton.ts
+++ creational/singleton/singleton.ts
@@ -11,8 +11,3 @@ export class Singleton {
     return Singleton.instance;
   }
 }
-
-const singleton1 = Singleton.getInstance();
-const singleton2 = Singleton.getInstance();
-
-console.log(singleton1 === singleton2); // true
